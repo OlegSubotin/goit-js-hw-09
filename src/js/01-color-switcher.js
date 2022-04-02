d68c7810483bdb8223d87fbccfe8c6e58df4edda
OlegSubotin <subotinoleg0@gmail.com>
2022-04-02 12:32:33 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const bodyRef = document.querySelector('body');
+
+let timerId = null;
+const INTERVAL_DURATION = 1000;
+startBtn.disabled = false;
+stopBtn.disabled = true;
+
+startBtn.addEventListener('click', onStartBtnClick);
+stopBtn.addEventListener('click', onStopBtnClick);
+
+function updateBodyColor() {
+    bodyRef.style.backgroundColor = getRandomHexColor();
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onStartBtnClick() {
+    console.log('on start button click');
+    timerId = setInterval(updateBodyColor, INTERVAL_DURATION);
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+}
+
+function onStopBtnClick() {
+    console.log('on stop button click');
+    clearInterval(timerId);
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+}
 
